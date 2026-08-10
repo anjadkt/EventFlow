d0ea7ff11d36510ddcc6eccece59f2b1986edb55
anjadkt <anjad076@gmail.com>
2026-08-10 11:55:36 +0530

    feat : next application inital setup completed

---
+++ client/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Event Flow",
+  description: "Event Flow is your ultimate guide to discovering live events, hidden gems, and community gatherings happening right around you. Whether you want a last-minute concert ticket, a weekend food festival, or a local run club, this app connects you to your city in real time.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
